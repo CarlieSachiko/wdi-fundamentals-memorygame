da62408c14595541aee04b3be7fae69b6ff03075
Carlie <carliesachiko@MacBook-Pro.local>
2016-11-22 18:45:07 -0800

    added cards via JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne===cardTwo) {
+/*if (cardOne===cardTwo) {
 alert("You found a match!");
 }
 
@@ -14,4 +14,21 @@ alert("You found a match!");
 
 if (cardOne===cardFour){
 alert("Sorry, try again.");
+}*/
+
+var gameBoard = document.getElementById('game-board');
+var cards = 4;
+
+var createCards = function () {
+	for (var i=0; i<cards; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className='card';
+	gameBoard.appendChild(cardElement);
+	}
 }
+var button= document.querySelector('button');
+button.addEventListener('click', createCards);
+
+
+
+
