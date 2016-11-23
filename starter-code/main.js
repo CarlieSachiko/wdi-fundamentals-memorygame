760788590bf670b48291006b058da1e9218fad7a
Carlie <carliesachiko@MacBook-Pro.local>
2016-11-23 12:22:19 -0800

    created game logic with arrays

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+
 
 /*if (cardOne===cardTwo) {
 alert("You found a match!");
@@ -17,17 +15,44 @@ alert("Sorry, try again.");
 }*/
 
 var gameBoard = document.getElementById('game-board');
-var cards = 4;
 
-var createCards = function () {
-	for (var i=0; i<cards; i++) {
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
 	var cardElement = document.createElement('div');
-	cardElement.className='card';
+	cardElement.className = 'card';
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+	gameBoard.appendChild(cardElement);
 	gameBoard.appendChild(cardElement);
 	}
 }
-var button= document.querySelector('button');
-button.addEventListener('click', createCards);
+
+function isTwoCards(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card')==='king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+	if (cardsInPlay.length===2) {
+		isMatch(cardsInPlay);
+		cardsInPlay =[];
+	}
+}
+
+function isMatch (cards) {
+	if (cards[0]===cards[1]) {
+		alert ("You found a match!");
+	} else {
+		alert ("sorry,try again.");
+	}
+}
+
+createBoard();
+
+
+
 
 
 
